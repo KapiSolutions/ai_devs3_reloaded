14a55dfb3dea6dd8323698b07e10355de179c83f
Kapisolutions <biuro@kapisolutions.pl>
2025-05-13 11:03:43 +0200

    feat: handle s01e01 mission

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,13 @@
 import express from 'express'
 import cors from 'cors'
+import s01Routes from './routes/s01.routes'
 
 const app = express()
 
 app.use(express.json())
 app.use(cors())
+
+app.use('/s01', s01Routes)
 
 app.get('/', (_, res) => {
 	res.send('AiDevs3 API is running')

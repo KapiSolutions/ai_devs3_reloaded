b8983019c27021f02cfe7540ea4df060223f7578
Kapisolutions <biuro@kapisolutions.pl>
2025-05-14 10:47:16 +0200

    chore: add new envs

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -1,8 +1,10 @@
 export const PORT = process.env.PORT || 3000
 export const OPENAI_API_KEY = process.env.OPENAI_API_KEY || ''
+export const AIDEVS_API_KEY = process.env.AIDEVS_API_KEY || ''
 export const ROBOTS_PORTAL_URL = process.env.ROBOTS_PORTAL_URL || ''
+export const CENTRALA_URL = process.env.CENTRALA_URL || ''
 
-const requiredEnvVars = ['OPENAI_API_KEY', 'ROBOTS_PORTAL_URL']
+const requiredEnvVars = ['OPENAI_API_KEY', 'ROBOTS_PORTAL_URL', 'AIDEVS_API_KEY', 'CENTRALA_URL']
 
 export function validateEnvs() {
 	const missingVars = requiredEnvVars.filter((varName) => !process.env[varName])

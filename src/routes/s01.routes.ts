e1ba45186f1cebc90d7c983ffaf4381cfe3c44db
Kapisolutions <biuro@kapisolutions.pl>
2025-05-16 13:05:29 +0200

    feat: handle s01e05 mission

--- src/routes/s01.routes.ts
+++ src/routes/s01.routes.ts
@@ -2,11 +2,13 @@ import { Router } from 'express'
 import playE01 from '../controllers/s01/e01.controller'
 import playE02 from '../controllers/s01/e02.controller'
 import playE03 from '../controllers/s01/e03.controller'
+import playE05 from '../controllers/s01/e05.controller'
 
 const router = Router()
 
 router.get('/e01', playE01)
 router.get('/e02', playE02)
 router.get('/e03', playE03)
+router.get('/e05', playE05)
 
 export default router

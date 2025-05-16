e6dcf3ac96b434c06b5c97c9735857f4f07211c0
Kapisolutions <biuro@kapisolutions.pl>
2025-05-16 12:58:25 +0200

    chore: update envs

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -3,6 +3,7 @@ export const OPENAI_API_KEY = process.env.OPENAI_API_KEY || ''
 export const AIDEVS_API_KEY = process.env.AIDEVS_API_KEY || ''
 export const ROBOTS_PORTAL_URL = process.env.ROBOTS_PORTAL_URL || ''
 export const CENTRALA_URL = process.env.CENTRALA_URL || ''
+export const ALLOWED_ORIGINS = process.env.ALLOWED_ORIGINS || ''
 
 const requiredEnvVars = ['OPENAI_API_KEY', 'ROBOTS_PORTAL_URL', 'AIDEVS_API_KEY', 'CENTRALA_URL']
 

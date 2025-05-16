2beb4ee8f2355074df65bf1e969cb93ad920a6a8
Kapisolutions <biuro@kapisolutions.pl>
2025-05-16 13:04:54 +0200

    chore: update app config

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,29 @@
-import express from 'express'
+import express, { Request, Response } from 'express'
 import cors from 'cors'
 import s01Routes from './routes/s01.routes'
+import helmet from 'helmet'
+import { corsOptions, rateLimiter } from './config/server.config'
 
 const app = express()
 
+app.use(helmet())
+app.use(cors(corsOptions))
 app.use(express.json())
-app.use(cors())
+app.use('/api', rateLimiter)
 
-app.use('/s01', s01Routes)
+// App routes
+app.use('/api/s01', s01Routes)
 
-app.get('/', (_, res) => {
+// Root route
+app.get('/', (_, res: Response) => {
 	res.send('AiDevs3 API is running')
 })
 
+// Handle 404 - Route not found
+app.use('*', (req: Request, res: Response) => {
+	res.status(404).json({
+		status: '❌ Error',
+		message: `Can't find ${req.originalUrl} on this server!`
+	})
+})
 export default app
